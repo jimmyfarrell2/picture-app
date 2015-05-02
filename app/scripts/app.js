a71138868204e181593756adbeac3beed3296b37
Jimmy Farrell <jfarrell22@gmail.com>
2015-05-02 13:17:53 -0400

    Copied over files from previous repos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic-timepicker'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -29,55 +29,100 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+      .state('tab.create', {
+        url: '/create',
+        views: {
+          'tab-create': {
+            templateUrl: 'templates/tab-create.html',
+            controller: 'CreateCtrl'
+          }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+      })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+      .state('tab.stories', {
+        url: '/stories',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-stories.html',
+            controller: 'StoriesCtrl'
+          }
+        }
+      })
+    //.state('tab.chat-detail', {
+    //  url: '/chats/:chatId',
+    //  views: {
+    //    'tab-chats': {
+    //      templateUrl: 'templates/chat-detail.html',
+    //      controller: 'ChatDetailCtrl'
+    //    }
+    //  }
+    //})
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/create');
 
+})
+
+.directive('standardTimeMeridian', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: {
+      etime: '=etime'
+    },
+    template: "<strong>{{stime}}</strong>",
+    link: function(scope, elem, attrs) {
+
+      scope.stime = epochParser(scope.etime, 'time');
+
+      function prependZero(param) {
+        if (String(param).length < 2) {
+          return "0" + String(param);
+        }
+        return param;
+      }
+
+      function epochParser(val, opType) {
+        if (val === null) {
+          return "00:00";
+        } else {
+          var meridian = ['AM', 'PM'];
+
+          if (opType === 'time') {
+            var hours = parseInt(val / 3600);
+            var minutes = (val / 60) % 60;
+            var hoursRes = hours > 12 ? (hours - 12) : hours;
+
+            var currentMeridian = meridian[parseInt(hours / 12)];
+
+            return (prependZero(hoursRes) + ":" + prependZero(minutes) + " " + currentMeridian);
+          }
+        }
+      }
+
+      scope.$watch('etime', function(newValue, oldValue) {
+        scope.stime = epochParser(scope.etime, 'time');
+      });
+
+    }
+  };
 });
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,20 +1,111 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('CreateCtrl', function ($scope, Stories, Camera) {
+      var fb = new Firebase('https://storyapp.firebaseio.com/');
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+      fb.on('value', function(dataSnapshot) {
+        console.log('data snapshot', dataSnapshot);
+        $scope.lastPic = window.atob(dataSnapshot.val().image);
+      });
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+      $scope.storyIsActive = false;
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+      $scope.startSlots = {epochTime: 12600, format: 12, step: 15};
+      $scope.endSlots = {epochTime: 12600, format: 12, step: 15};
+
+      $scope.toggleScheduler = function(){
+        $scope.showScheduleOptions = !($scope.showScheduleOptions);
+      };
+
+      var schedule = function (start, end, interval) {
+
+        if (!start) {
+          start = new Date().getTime();
+        }
+
+        if (!interval) {
+          interval = 1200000;
+        } else {
+          interval = interval * 60000;
+        }
+
+        var nudge = new Date(start + interval);
+        console.log('duh');
+
+        cordova.plugins.notification.local.schedule({
+          at: nudge,
+          badge: 1,
+          text: 'Picture Time!'
+        });
+
+        console.log('scheduled!')
+
+      };
+
+      $scope.startStory = function (start, end, interval) {
+
+        $scope.storyIsActive = true;
+
+        schedule(start, end, interval);
+
+
+        Stories.startStory(start, end).then(function (data) {
+          console.log('Story has started.');
+          console.log('data', data);
+        });
+      };
+
+      $scope.endStory = function () {
+        Stories.endStory().then(function (data) {
+          console.log('Story has ended.');
+          console.log('data', data);
+          $scope.storyIsActive = false;
+        });
+      };
+
+      var onComplete = function(error) {
+        if (error) {
+          console.log('Upload failed');
+        } else {
+          console.log('Upload succeeded');
+        }
+      };
+
+      var cameraOptions = {
+        quality: 75,
+        targetWidth: 320,
+        targetHeight: 320,
+        destinationType: 0,
+        sourceType: 1,
+        allowEdit: true,
+        encodingType: 0,
+        saveToPhotoAlbum: false
+      };
+
+      $scope.takePhoto = function () {
+        Camera.getPicture(cameraOptions).then(function (base64Image) {
+            fb.set({image: base64Image}, onComplete);
+        }, function (err) {
+          console.err(err);
+        });
+      };
+
+    })
+
+    .controller('StoriesCtrl', function ($scope, Chats) {
+      $scope.chats = Chats.all();
+      $scope.remove = function (chat) {
+        Chats.remove(chat);
+      };
+    })
+
+//.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//  $scope.chat = Chats.get($stateParams.chatId);
+//})
+
+    .controller('AccountCtrl', function ($scope) {
+      $scope.settings = {
+        enableFriends: true
+      };
+    });
+
